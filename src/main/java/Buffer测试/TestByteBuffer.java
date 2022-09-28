ffd09a38ee2249c1ff13ccad3f32df95327a6b8e
Pinkman119 <1191891166@qq.com>
2022-09-28 09:32:14 +0800

    2022年9月28日 09:31:46

---
+++ src/main/java/Buffer测试/TestByteBuffer.java
@@ -0,0 +1,50 @@
+package Buffer测试;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Topic：
+ *
+ * @Author:Pinkman
+ * @Date:2022/9/27 20:23
+ * @Tips:
+ */
+public class TestByteBuffer {
+    public static void main(String[] args) {
+        // FileChannel
+        // 1.输入输出流获取
+        try {
+            FileChannel channel = new FileInputStream("data.txt").getChannel();
+
+            // 准备缓冲区，向内存中申请
+            ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+
+            while (true){
+                int len = channel.read(byteBuffer);
+
+                if(len == -1){
+                    break;
+                }
+                // 从channel中读取数据，向buffer写入
+                channel.read(byteBuffer);
+
+                byteBuffer.flip();// 切换为读模式
+
+                // 是否还有剩余未读数据
+                while (byteBuffer.hasRemaining()){
+                    byte b = byteBuffer.get();
+                }
+                byteBuffer.clear();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 2.RandomAccessFile获取
+    }
+}
